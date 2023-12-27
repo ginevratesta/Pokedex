2495740c6b5e5fcc1ee18a0d872462a6b8285c38
Ginevra Testa <ginevra99testaa@gmail.com>
2023-12-27 17:44:37 +0100

    adjustments

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const pokedex = document.querySelector("#poke-carousel");
+const display = document.querySelector(".img-box");
 const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+let currentImageURL = ''; // Variable to track the current displayed image URL
+let currentIndex = 0; // Variable to track the current index of the displayed card
+
+// Function to update the display with the selected Pokémon image
+function updateDisplay(imageURL) {
+  display.innerHTML = `<img class="w-100 h-100" src="${imageURL}" alt="Selected Pokemon"/>`;
+  currentImageURL = imageURL;
+}
 
 function fetchPokemonData() {
   for (let id = 387; id <= 493; id++) {
@@ -7,24 +16,57 @@ function fetchPokemonData() {
     fetch(url)
       .then(res => res.json())
       .then(pokemonData => {
-        console.log(pokemonData)
         pokedex.innerHTML += `
-        <div class="poke-card">
-            <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
-            <p>Pokemon: ${pokemonData.name.toUpperCase()}</p>
-           </div>
+          <div class="poke-card d-flex align-items-center px-3 mb-2" data-image="${pokemonData.sprites.front_default}" data-index="${id - 387}">
+            <div class="carousel-pokeball me-3">
+              <img class="w-100 h-100" src="./assets/red-poke.png" alt"red pokeball"/>
+            </div>
+            <div class="carousel-img me-3">
+              <img class="w-100 h-100" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
+            </div>
+            <div>
+              <p class="pt-3">${pokemonData.name.toUpperCase()}</p>
+            </div>
+          </div>
         `;
+
+        const pokeCards = document.querySelectorAll('.poke-card');
+        pokeCards.forEach(card => {
+          card.addEventListener('click', () => {
+            const imageURL = card.getAttribute('data-image');
+            updateDisplay(imageURL);
+          });
+        });
+
+        if (id === 387) {
+          updateDisplay(pokemonData.sprites.front_default);
+        }
       })
       .catch(error => {
         console.error(`Error fetching data for Pokemon with ID ${id}:`, error);
       });
   }
+
+  // Adding keyboard event listener for navigation
+  document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowUp') {
+      // Logic to navigate up (display previous card)
+      if (currentIndex > 0) {
+        currentIndex -= 1;
+        const prevCard = document.querySelector(`[data-index="${currentIndex}"]`);
+        const imageURL = prevCard.getAttribute('data-image');
+        updateDisplay(imageURL);
+      }
+    } else if (e.key === 'ArrowDown') {
+      // Logic to navigate down (display next card)
+      if (currentIndex < 106) {
+        currentIndex += 1;
+        const nextCard = document.querySelector(`[data-index="${currentIndex}"]`);
+        const imageURL = nextCard.getAttribute('data-image');
+        updateDisplay(imageURL);
+      }
+    }
+  });
 }
 
 fetchPokemonData();
-
-        // <div class="col-12 col-sm-12 col-md-4 col-lg-3 d-flex flex-column align-items-center">
-        //   <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
-        //   <p>Pokemon: ${pokemonData.name.toUpperCase()}</p>
-        //   <p>Type: ${pokemonData.types[0].type.name}</p>
-        // </div>
