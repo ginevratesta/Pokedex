c7e4b56b05504c9e28a8125e3312846a23e0d060
Ginevra Testa <ginevra99testaa@gmail.com>
2023-12-27 15:53:09 +0100

    carousel

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-// const pokedex = document.querySelector(".row");
-// const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
+const pokedex = document.querySelector("#poke-carousel");
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/';
 
-// function fetchPokemonData() {
-//   for (let id = 387; id <= 493; id++) {
-//     const url = `${baseURL}${id}`;
-//     fetch(url)
-//       .then(res => res.json())
-//       .then(pokemonData => {
-//         console.log(pokemonData)
-//         pokedex.innerHTML += `
-//           <div class="col-12 col-sm-12 col-md-4 col-lg-3 d-flex flex-column align-items-center">
-//             <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
-//             <p>Pokemon: ${pokemonData.name.toUpperCase()}</p>
-//             <p>Type: ${pokemonData.types[0].type.name}</p>
-//           </div>
-//         `;
-//       })
-//       .catch(error => {
-//         console.error(`Error fetching data for Pokemon with ID ${id}:`, error);
-//       });
-//   }
-// }
+function fetchPokemonData() {
+  for (let id = 387; id <= 493; id++) {
+    const url = `${baseURL}${id}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(pokemonData => {
+        console.log(pokemonData)
+        pokedex.innerHTML += `
+        <div class="poke-card">
+            <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
+            <p>Pokemon: ${pokemonData.name.toUpperCase()}</p>
+           </div>
+        `;
+      })
+      .catch(error => {
+        console.error(`Error fetching data for Pokemon with ID ${id}:`, error);
+      });
+  }
+}
 
-// fetchPokemonData();
+fetchPokemonData();
+
+        // <div class="col-12 col-sm-12 col-md-4 col-lg-3 d-flex flex-column align-items-center">
+        //   <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}"/>
+        //   <p>Pokemon: ${pokemonData.name.toUpperCase()}</p>
+        //   <p>Type: ${pokemonData.types[0].type.name}</p>
+        // </div>
